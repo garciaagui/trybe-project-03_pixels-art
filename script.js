83f2f151c454b7ad3ab1e064d58a4c41c78b401f
Guilherme Garcia <garciaguig@gmail.com>
2022-05-25 16:23:16 -0300

    Conclusão do Requisito 4

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const colorPallete = document.getElementById('color-palette');
+const pixelsBoard = document.getElementById('pixel-board');
 
 function createColorPallete() {
   for (let i = 1; i <= 4; i += 1) {
@@ -16,5 +17,23 @@ function setColors() {
   createdPalleteElements[3].style.backgroundColor = '#E76F51';
 }
 
+function generatePixels() {
+  for (let i = 1; i <= 25; i += 1) {
+    const newPixel = document.createElement('section');
+    newPixel.setAttribute('class', 'pixel');
+    pixelsBoard.appendChild(newPixel);
+  }
+}
+
+const pixelsCreated = document.getElementsByClassName('pixel');
+
+function whiteColorOnload() {
+  for (let i = 0; i < pixelsCreated.length; i += 1) {
+    pixelsCreated[i].style.backgroundColor = 'white';
+  }
+}
+
 createColorPallete();
 setColors();
+generatePixels();
+window.addEventListener('load', whiteColorOnload);
