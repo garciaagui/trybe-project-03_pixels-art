323c23509ab5c268b112e593aec6afe6c3adf546
Guilherme Garcia <garciaguig@gmail.com>
2022-06-05 15:31:35 -0300

    Melhorias no script.js (Code Refactoring)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Definição de Variáveis;
+// Definição das Variáveis;
 const paletteElements = document.getElementsByClassName('color');
 const generatePaletteButton = document.getElementById('another-palette');
 const pixelsBoard = document.getElementById('pixel-board');
@@ -18,32 +18,40 @@ function generateRandomColors() {
 }
 
 function setColors() {
-  paletteElements[0].style.backgroundColor = 'black';
-  paletteElements[1].style.backgroundColor = generateRandomColors();
-  paletteElements[2].style.backgroundColor = generateRandomColors();
-  paletteElements[3].style.backgroundColor = generateRandomColors();
+  for (let i = 0; i < paletteElements.length; i += 1) {
+    if (i !== 0) {
+      paletteElements[i].style.backgroundColor = generateRandomColors();
+    } else {
+      paletteElements[i].style.backgroundColor = 'black';
+    }
+  }
 }
 
 function selectBlackColorFirst() {
   paletteElements[0].classList.add('selected');
 }
 
-function changePainter(event) {
-  const hadSelectedClass = document.querySelector('.selected');
-  hadSelectedClass.classList.remove('selected');
-  event.target.classList.add('selected');
+function changePainter(e) {
+  const lastSelected = document.querySelector('.selected');
+  lastSelected.classList.remove('selected');
+  e.target.classList.add('selected');
 }
 
-function paintPixel(event) {
-  const pixel = event;
+function paintPixel(e) {
   const colorSelected = document.querySelector('.selected').style.backgroundColor;
-  pixel.target.style.backgroundColor = colorSelected;
+  e.target.style.backgroundColor = colorSelected;
 }
 
-function generatePixels() {
-  for (let i = 1; i <= 5; i += 1) {
+function whiteAllPixels() {
+  for (let i = 0; i < pixelsCreated.length; i += 1) {
+    pixelsCreated[i].style.backgroundColor = 'white';
+  }
+}
+
+function generatePixels(number) {
+  for (let i = 1; i <= number; i += 1) {
     const newPixelLine = document.createElement('section');
-    for (let index = 1; index <= 5; index += 1) {
+    for (let index = 1; index <= number; index += 1) {
       const newPixel = document.createElement('section');
       newPixel.setAttribute('class', 'pixel');
       newPixelLine.appendChild(newPixel);
@@ -53,12 +61,7 @@ function generatePixels() {
   for (let i = 0; i < pixelsCreated.length; i += 1) {
     pixelsCreated[i].addEventListener('click', paintPixel);
   }
-}
-
-function whiteAllPixels() {
-  for (let i = 0; i < pixelsCreated.length; i += 1) {
-    pixelsCreated[i].style.backgroundColor = 'white';
-  }
+  whiteAllPixels();
 }
 
 function removePixelBoardChilds() {
@@ -68,38 +71,27 @@ function removePixelBoardChilds() {
 }
 
 function checkInputLimitValues(value) {
-  let inputValue = value;
-  if (generateBoardInput.value > 0 && generateBoardInput.value < 5) {
-    inputValue = 5;
+  const inputValue = generateBoardInput.value;
+  let newBoardSize = value;
+  if (inputValue > 0 && inputValue < 5) {
+    newBoardSize = 5;
     window.alert('Valor inserido menor que o permitido. Consideramos 5.');
-  } else if (generateBoardInput.value > 50) {
-    inputValue = 50;
+  } else if (inputValue > 50) {
+    newBoardSize = 50;
     window.alert('Valor inserido maior que o permitido. Consideramos 50.');
   } else {
-    inputValue = generateBoardInput.value;
+    newBoardSize = inputValue;
   }
-  return inputValue;
+  return newBoardSize;
 }
 
 function changeBoardSize() {
   const inputValue = checkInputLimitValues();
   removePixelBoardChilds();
-  for (let i = 1; i <= inputValue; i += 1) {
-    const newPixelLine = document.createElement('section');
-    for (let index = 1; index <= inputValue; index += 1) {
-      const newPixel = document.createElement('section');
-      newPixel.setAttribute('class', 'pixel');
-      newPixel.style.backgroundColor = 'white';
-      newPixelLine.appendChild(newPixel);
-    }
-    pixelsBoard.appendChild(newPixelLine);
-  }
-  for (let i = 0; i < pixelsCreated.length; i += 1) {
-    pixelsCreated[i].addEventListener('click', paintPixel);
-  }
+  generatePixels(inputValue);
 }
 
-function checkInputEmptyValue() {
+function checkInputValueValidity() {
   if (generateBoardInput.value === 0 || generateBoardInput.value === '') {
     window.alert('Board inválido!');
   } else {
@@ -108,13 +100,14 @@ function checkInputEmptyValue() {
 }
 
 // Ativação das Funções;
-setColors();
-generatePixels();
-window.addEventListener('load', whiteAllPixels);
-window.addEventListener('load', selectBlackColorFirst);
-generatePaletteButton.addEventListener('click', setColors);
+window.addEventListener('load', () => {
+  setColors();
+  generatePixels(5);
+  selectBlackColorFirst();
+});
 for (let i = 0; i < paletteElements.length; i += 1) {
   paletteElements[i].addEventListener('click', changePainter);
 }
+generatePaletteButton.addEventListener('click', setColors);
 clearButton.addEventListener('click', whiteAllPixels);
-generateBoardButton.addEventListener('click', checkInputEmptyValue);
+generateBoardButton.addEventListener('click', checkInputValueValidity);
